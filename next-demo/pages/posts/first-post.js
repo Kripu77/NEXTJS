a2371b350a965ae5609d14ce926ebc8c2d16c1f9
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-11-26 20:28:21 +1100

    getStaticProps

--- next-demo/pages/posts/first-post.js
+++ next-demo/pages/posts/first-post.js
@@ -2,20 +2,22 @@ import Link from "next/dist/client/link";
 import Image from "next/dist/client/image";
 import pic from "../../public/images/profile.jpg"
 import Head from "next/dist/shared/lib/head";
+import Layout from "../components/Layout";
 
 export default function FirstPost (){
     return (
       <>
-      <Head>
-        <title>First Post</title>
-      </Head>
-        <h1> This is our first post</h1>
-        <Link href="/">
-         
-          <a> Back to Home</a>
-        </Link>
-        {/* image component */}
-        <Image src={pic}  alt="Your Name"/>
+        <Layout>
+          <Head>
+            <title>First Post</title>
+          </Head>
+          <h1> This is our first post</h1>
+          <Link href="/">
+            <a> Back to Home</a>
+          </Link>
+          {/* image component */}
+          <Image src={pic} alt="Your Name" />
+        </Layout>
       </>
     );  
 }
