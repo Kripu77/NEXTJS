419393b7d6ca9111431a0dade0817667cbd8e9e8
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-12-04 10:59:14 +1100

    params fix

--- next-demo/pages/SSGDynamic/[postID].js
+++ next-demo/pages/SSGDynamic/[postID].js
@@ -12,8 +12,9 @@ export default Post
 
 //we want to pre-render the data so instead of useEffect we will use getStaticProps
 export async function getStaticProps({id}){
+    const{params} = id
 
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`)
     const data = response.json()
 
     return{props:{
